5f8ef6149757ab5e52fdc44ec9573ee6e5e63339
Javid Rafiee <javid.rafiee@gmail.com>
2020-12-28 15:59:37 +0330

    3.Dependency Injection with Spring -> 4.The Spring Context

---
+++ src/main/java/com/javid/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,21 @@
+package com.javid.springframework.sfgdi;
+
+import com.javid.springframework.sfgdi.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) context.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
+	}
+
+}
---
+++ src/main/java/com/javid/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,13 @@
+package com.javid.springframework.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello Spring Boot :)");
+
+        return "Greeting don";
+    }
+}
