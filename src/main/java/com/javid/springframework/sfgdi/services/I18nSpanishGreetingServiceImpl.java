28906d05c3c04c792b288976bf98e8f4a4066832
Javid Rafiee <javid.rafiee@gmail.com>
2020-12-28 22:26:35 +0330

    3.Dependency Injection with Spring -> 10.Spring Profiles

--- src/main/java/com/javid/springframework/sfgdi/services/I18nSpanishGreetingServiceImpl.java
+++ src/main/java/com/javid/springframework/sfgdi/services/I18nSpanishGreetingServiceImpl.java
@@ -3,11 +3,11 @@ package com.javid.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("SP")
+@Profile("ES")
 @Service("I18nService")
 public class I18nSpanishGreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hola Mundo - SP";
+        return "Hola Mundo - ES";
     }
 }
